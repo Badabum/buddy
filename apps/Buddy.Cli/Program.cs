799fefa9ebfaf1c72289d0cc742b9275259e87bf
Ihor Korotenko <ihor.korotenko@occtoo.com>
2023-05-04 10:53:52 +0200

    Initial commit with cli app

---
+++ apps/Buddy.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Spectre.Console.Cli;
+
+var app = new CommandApp();
+
+app.Configure(config =>
+{
+});
+
+await app.RunAsync(args);
